dc6c5f89dc5ed9bbdad57d35a1f7dd76214014e8
MurmanProg <marchenko2.va@spbstu.ru>
2021-03-14 09:28:57 +0300

    maven

---
+++ src/main/kotlin/ru/marchenko/notes/api/NotesApiApplication.kt
@@ -0,0 +1,12 @@
+package ru.marchenko.notes.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotesApiApplication
+
+//ovff
+fun main(args: Array<String>) {
+	runApplication<NotesApiApplication>(*args)
+}
